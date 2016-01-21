1be962051ed2c3b31790ba59c2285171bfba95eb
Rainrider <rainrider.wow@gmail.com>
2016-01-21 23:44:09 +0100

    Test for the event unit

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -33,16 +33,16 @@ dataobj.Test = function()
 	return IconLine(icon, spell)
 end
 
-dataobj.Scan = function()
-	dataobj.player = dataobj.Test()
-end
-
 ae.RegisterEvent(dataobj, "PLAYER_TALENT_UPDATE", function()
 	if not IsPlayerSpell(EXOTIC_MUNITIONS_ID) then
 		ae.UnregisterEvent(dataobj, "UNIT_AURA")
 		dataobj.player = nil
 	else
-		ae.RegisterEvent(dataobj, "UNIT_AURA", dataobj.Scan)
+		ae.RegisterEvent(dataobj, "UNIT_AURA", function(event, name)
+			if unit == "player" then
+				dataobj.player = dataobj.Test()
+			end
+		end)
 		dataobj.player = dataobj.Test()
 	end
 end)
