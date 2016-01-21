7a2ea9add7cc8b91f2d1ab02a122cbe4d76eecc8
Rainrider <rainrider.wow@gmail.com>
2016-01-21 23:23:09 +0100

    Fix Exotic Munitions
    Register PLAYER_TALENT_UPDATE.
    Stop the update if the player does not have the talent.

--- modules/Hunter.lua
+++ modules/Hunter.lua
@@ -1,11 +1,50 @@
-
 local myname, Cork = ...
 if Cork.MYCLASS ~= "HUNTER" then return end
 
+local ae = LibStub("AceEvent-3.0")
+local IconLine = Cork.IconLine
 
--- Exotic Munitions
-Cork:GenerateAdvancedSelfBuffer("Exotic Munitions", {162537, 162536, 162539})
+local EXOTIC_MUNITIONS_ID = 162534
+local munitionids = {
+	162537,
+	162536,
+	162539,
+}
 
+munitionnames, munitionicons = {}, {}
+for i = 1, #munitionids do
+	local id = munitionids[i]
+	local name, _, icon = GetSpellInfo(id)
+	munitionnames[id] = name
+	munitionicons[name] = icon
+end
+
+local dataobj = Cork:GenerateAdvancedSelfBuffer("Exotic Munitions", munitionids)
+
+dataobj.Test = function()
+	if not Cork.dbpc["Exotic Munitions-enabled"] or (IsResting() and not Cork.db.debug) or not IsPlayerSpell(EXOTIC_MUNITIONS_ID) then
+		return
+	end
+	for _, spellname in pairs(munitionnames) do
+		if (UnitBuff("player", spellname)) then return end
+	end
+	local spell = Cork.dbpc["Exotic Munitions-spell"]
+	local icon = munitionicons[spell]
+	return IconLine(icon, spell)
+end
+
+dataobj.Scan = function()
+	dataobj.player = dataobj.Test()
+end
+
+ae.RegisterEvent(dataobj, "PLAYER_TALENT_UPDATE", function()
+	if not IsPlayerSpell(EXOTIC_MUNITIONS_ID) then
+		ae.UnregisterEvent(dataobj, "UNIT_AURA")
+	else
+		ae.RegisterEvent(dataobj, "UNIT_AURA", dataobj.Scan)
+	end
+	dataobj.player = dataobj.Test()
+end)
 
 -- Trap Launcher
 local spellname, _, icon = GetSpellInfo(77769)
